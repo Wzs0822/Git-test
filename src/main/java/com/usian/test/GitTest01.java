7e865b60424d2baff9ffb18e60088eb838fe1a87
Wzs0822 <1601509967@qq.com>
2021-01-20 16:34:30 +0800

    GitTest.1.1

--- src/main/java/com/usian/test/GitTest01.java
+++ src/main/java/com/usian/test/GitTest01.java
@@ -2,6 +2,6 @@ package com.usian.test;
 
 public class GitTest01 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("这是一个Git测试项目！！");
     }
 }
